dd19cfc72687d44f54e37f7e89b20c3bbfbdb803
Zywl <86253343+opZywl@users.noreply.github.com>
2021-07-16 12:58:35 -0300

    Create EventModelUpdate.java

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/entity/impl/EventModelUpdate.java
@@ -0,0 +1,24 @@
+package net.ccbluex.liquidbounce.utils.entity.impl;
+
+import net.ccbluex.liquidbounce.utils.entity.EventCancellable;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class EventModelUpdate
+extends EventCancellable {
+    private final EntityPlayer player;
+    private final ModelPlayer model;
+
+    public EventModelUpdate(EntityPlayer player, ModelPlayer model) {
+        this.player = player;
+        this.model = model;
+    }
+
+    public final EntityPlayer getPlayer() {
+        return this.player;
+    }
+
+    public final ModelPlayer getModel() {
+        return this.model;
+    }
+}
